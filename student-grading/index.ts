e2f65e6d9ff4afa840122e474d634b36ab052ae3
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-08 17:55:30 +0300

    SOLVE student-grading

---
+++ student-grading/index.ts
@@ -0,0 +1,66 @@
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades: number[]): number[] {
+    const roundedGrades:number[] = [];
+    let rounedValue:number;
+    for (let i=0;i<grades.length ;i++){
+        if(grades[i]<38) {
+        roundedGrades.push(grades[i]);
+        continue;
+        }
+        rounedValue = Math.ceil(grades[i]/5) * 5;
+(rounedValue - grades[i]) < 3 ?  roundedGrades.push(rounedValue)             :roundedGrades.push(grades[i]);
+    
+    }
+    return roundedGrades;
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const gradesCount: number = parseInt(readLine().trim(), 10);
+
+    let grades: number[] = [];
+
+    for (let i: number = 0; i < gradesCount; i++) {
+        const gradesItem: number = parseInt(readLine().trim(), 10);
+
+        grades.push(gradesItem);
+    }
+
+    const result: number[] = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
