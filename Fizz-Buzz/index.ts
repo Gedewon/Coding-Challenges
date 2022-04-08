192d6759163bb1d5601d2255f49bc6fe858e3c31
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-08 15:54:57 +0300

    FIZZ BUZZ

---
+++ Fizz-Buzz/index.ts
@@ -0,0 +1,16 @@
+function fizzBuzz(n: number): string[] {
+    const answer = [];
+    for(let i=1;i<=n;i++){
+        if(i%5 == 0 && i%3 ==0 ){
+            answer.push("FizzBuzz");
+        }else if(i%5 == 0){
+            answer.push("Buzz");
+        }else if(i%3 == 0){
+            answer.push("Fizz")
+        }else{
+            answer.push(i.toString())
+        }
+    }
+    return answer;
+
+};
