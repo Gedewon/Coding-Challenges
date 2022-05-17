168c4c001728848e97fa865ccd4d88504aeee573
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-17 03:31:55 +0300

    SORTING-FIND-TARGET-BUBBLE

--- Sorting-Find-Target-Indices/index.ts
+++ Sorting-Find-Target-Indices/index.ts
@@ -1,8 +1,8 @@
 function targetIndices(nums: number[], target: number): number[] {
 
-
-    let targetIndex = [];
-     mergeSort(nums).forEach((el,index)=>{
+   
+    let targetIndex: number[] =[];
+     bubbleSort(nums).forEach((el,index)=>{
         if(el === target)
            targetIndex.push(index);
     })
@@ -10,34 +10,21 @@ function targetIndices(nums: number[], target: number): number[] {
     
 };
  
- function mergeSort(arr: number[]): number[] {
-     const half = arr.length / 2;
- 
-     // the base case is array length <=1
-     if (arr.length <= 1) {
-         return arr;
- 
+ function bubbleSort(arr){
+     
+     let isSorted = false;
+     
+     while(!isSorted){
+          isSorted = true;
+        arr.forEach((el,index,arr)=>{
+            if(el > arr[index+1]){
+              [arr[index],arr[index+1]] = [arr[index+1],arr[index]];
+                isSorted = false;
+            }
+              
+            
+        })
      }
- 
-     const left = arr.splice(0, half); // the first half of the array
-     const right = arr;
-     return merge(mergeSort(left), mergeSort(right));
+    return arr; 
  }
  
- function merge(left :number[] , right  :number[]) :number[] {
-     let sortedArr = []; // the sorted elements will go here
- 
-     while (left.length && right.length) {
-         // insert the smallest element to the sortedArr
-         if (left[0]  < right[0])  {
-             sortedArr.push(left.shift());
-         } else {
-             sortedArr.push(right.shift());
-         }
-     }
- 
-     // use spread operator and create a new array, combining the three arrays
-     return [...sortedArr, ...left, ...right];
- }
-
- console.log(targetIndices([1,5,3,2,7,2,9],2));
