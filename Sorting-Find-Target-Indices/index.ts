d9c2c79777241d1cc3a6326fca83a0696297049d
gedewon <gedewon.hayle.9@gmail.com>
2022-07-19 16:34:15 +0300

    2089. Find Target Indices After Sorting Array

--- Sorting-Find-Target-Indices/index.ts
+++ Sorting-Find-Target-Indices/index.ts
@@ -5,7 +5,8 @@ function targetIndices(nums: number[], target: number): number[] {
      bubbleSort(nums).forEach((el,index)=>{
         if(el === target)
            targetIndex.push(index);
-    })
+     })
+  //runs O(n) = n^2
     return targetIndex;
     
 };
@@ -28,3 +29,39 @@ function targetIndices(nums: number[], target: number): number[] {
     return arr; 
  }
  
+
+
+function targetIndicesMerge(nums: number[], target: number): number[] {
+
+  let SortedArray = Array.from(nums).sort((a,b)=>a-b);  //Sort in count to run it in n +r time 
+  let countIndex = [];
+ 
+   for(let i=0;i<nums.length;i++){
+       
+   
+       while(SortedArray[i]==target){
+           countIndex.push(i);
+           i++
+       }
+       if(countIndex.length)break;
+           
+   };
+ 
+ //runs in O(n) =nlogn
+ return countIndex;    
+};
+
+function countSort(inputArray){
+
+let newArray = Array(inputArray.length)
+             .fill(0);
+
+inputArray.forEach(element=>{
+     newArray[element]=++newArray[element] || 1;
+});
+  //runs in O(n) = n+r 
+return newArray.map((element,index)=>Array(element)
+                           .fill(index))
+                           .flat()
+                           .filter(Boolean);
+}
