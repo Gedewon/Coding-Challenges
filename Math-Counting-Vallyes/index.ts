7eda94fb5af26e7a0b4fd77cbd454eedd0c1ff2d
gedewon <gedewon.hayle.9@gmail.com>
2022-08-03 04:39:58 +0300

    Counting Valleys

---
+++ Math-Counting-Vallyes/index.ts
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+    let pathlist=path.split('');
+    let numberOfvalues = 0 ,count =0; 
+    for(let i = 0 ; i < pathlist.length; i++){
+        if(pathlist[i] == 'U')count++;
+        else count--;
+        if(count === 0 && pathlist[i] == 'U')numberOfvalues++;
+    }
+return numberOfvalues;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
